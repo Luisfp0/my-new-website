b9c9257b534832bbad899026d60d093c68b03765
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-02-27 20:12:11 -0300

    feat: tailWind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="text-center">
-      <header className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-xl text-white">
-        <span className="text-2xl">teste</span>
-      </header>
+    <div>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-black w-full h-16">
+      <span className="text-white">Header</span>
+    </header>
+  );
+}
+
+export default Header;
