3e00834576e5d383d6c7a0352c0775619c6d22b9
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-02-28 17:04:45 -0300

    feat: hero page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div className="bg-black w-screen h-screen">
+    <div className="bg-black w-screen h-screen flex flex-col">
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,17 @@
+import { RiFacebookBoxFill } from "react-icons/ri";
+
+function Hero() {
+  return (
+    <div className="flex justify-center items-center bg-green w-7/12 h-screen mx-auto max-w-1150">
+      <div>
+        <figure>
+          <a>
+            <RiFacebookBoxFill />
+          </a>
+        </figure>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
