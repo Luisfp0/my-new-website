dc8e89ac47b00a8a063a2b0202b193fd0e833697
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-03-01 13:42:46 -0300

    feat: presentation box

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -6,21 +6,25 @@ const COLOR = "#2ccb69";
 const options = {
   strings: [
     `I code cool <span style='color: ${COLOR}; text-shadow: rgba(0, 173, 109, 0.393) 10px 0px 10px;'>web apps</span>`,
+    `I code cool <span style='color: ${COLOR}; text-shadow: rgba(0, 173, 109, 0.393) 10px 0px 10px;'>mobile apps</span>`,
     `I love <span style='color: ${COLOR}; text-shadow: rgba(0, 173, 109, 0.393) 10px 0px 10px;'>javascript</span>`,
     `I love <span style='color: ${COLOR}; text-shadow: rgba(0, 173, 109, 0.393) 10px 0px 10px;'>web3</span>`,
   ],
-  typeSpeed: 50,
+  typeSpeed: 65,
   backSpeed: 30,
   loop: true,
-  cursorChar: '<span style="font-size: 60px; color: #2ccb69;">|</span>',
+  cursorChar: `<span style="font-size: 60px; color: ${COLOR}; padding-right: 32px">|</span>`,
 };
 
 function Presentation() {
   const ref = useRef(null);
   useTyped(ref, options);
   return (
-    <div>
-      <span className="text-6xl text-white" ref={ref} />
+    <div className="relative">
+      <div className="border-4 border-solid border-white inline-block relative">
+        <span className="text-6xl text-white py-8 pl-8 pr-2" ref={ref} />
+      </div>
+      <div className="absolute w-0 h-0 border-solid border-white border-b-19 border-l-19 border-l-transparent left-10 -bottom-4 transform rotate-180"></div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      borderWidth: {
+        19: "19px",
+        30: "30px",
+      },
+      inset: {
+        19: "19px",
+        29: "30px",
+      },
       colors: {
         "custom-gray-dark": "#3c3c3c",
         "custom-gray": "#333232",
