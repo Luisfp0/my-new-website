573e64531ca5319132c0ac681732d831b12cceba
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-03-06 17:54:10 -0300

    feat: hireMe

--- src/components/HireMe.tsx
+++ src/components/HireMe.tsx
@@ -9,12 +9,16 @@ function HireMe() {
   return (
     <div
       style={backgroundStyle}
-      className="max-w-[1150px] mx-auto justify-center items-center w-full h-[163px]"
+      className="max-w-[1150px] mx-auto justify-center items-center w-full h-[163px] relative"
     >
-      <div className="w-full h-full flex items-center justify-center bg-black bg-opacity-80 border-2 border-black">
+      <div className="w-full h-full flex-col flex items-center justify-center bg-black bg-opacity-80 border-2 border-custom-gray relative">
         <h3 className="text-[30px] text-white my-[16px]">
-          Let´s Work Together On Your Next Project!
+          Let's Work Together On Your Next Project!
         </h3>
+        <a className="text-white bg-custom-orange py-[12px] px-[20px] absolute -bottom-[22px]">
+          HIRE ME NOW!
+        </a>
+        <div className="absolute -bottom-[32px] left-[520px] w-0 h-0 border-solid border-custom-orange border-b-[10px] border-l-[10px] border-l-transparent transform rotate-180"></div>
       </div>
     </div>
   );
